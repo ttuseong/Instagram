574cec5bebb5c3ac76ec68c1568490b335dbc193
ttuseong <zlsp21128@naver.com>
2020-12-11 12:47:10 +0900

    메인 서브 컨텐츠에서 사용자 내용 추가  - 메인 서브 컨텐츠에서 사용자 내용 추가  - 화면 크기가 변경될 때 위치 이동을 위한 js 추가 및 jquery 파일 추가  - 화면 크기가 935보다 작아지면 서브 컨텐츠 안보이게 미디어 쿼리 적용

---
+++ assets/js/mainPage.js
@@ -0,0 +1,19 @@
+// 서브 컨텐츠 위치를 조정할 때 고정으로 필요한 값
+var basicLeft = $(".maincontents").outerWidth(true);
+
+$(document).ready(function(){
+  subcontentsMove();
+})
+
+//브라우저의 크기가 변경될 때 호출
+$(window).resize(function(){
+  subcontentsMove();
+});
+
+// 메인 페이지 서브 컨텐츠 위치 조정
+function subcontentsMove(){
+  var left = Number($('.maincontainer').css("margin-left").replace('px', '')) + basicLeft;
+
+  $('.subcontents').css('left', left+'px');
+  console.log(left);
+}
